01a934fef4de019ee15541d3509c626f5cb7f277
Zachary Yedidia <zyedidia@gmail.com>
2015-04-02 17:20:29 -0400

    Exit when requirements are not met

--- src/SFML.jl
+++ src/SFML.jl
@@ -4,12 +4,14 @@ function __init__()
 	if VERSION < v"0.4.0-dev"
 		println("You must have at least julia 0.4 to use this package")
 		println("You currently have ", VERSION)
+		exit(1)
 	end
 
 	try
 		Libdl.dlopen("libcsfml-graphics")
 	catch Exception
 		println("Something has gone wrong with the csfml installation. Please reinstall this package")
+		exit(1)
 	end
 
 	try
@@ -17,6 +19,7 @@ function __init__()
 	catch Exception
 		println("You must have the C++ library SFML installed to use this package")
 		println("Please install it")
+		exit(1)
 	end
 end
 
