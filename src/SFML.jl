3193450b007d9cbdcd6d4b739ac331d4a8d07766
Zachary Yedidia <zyedidia@gmail.com>
2015-04-02 17:11:49 -0400

    Check version and libraries

--- src/SFML.jl
+++ src/SFML.jl
@@ -5,6 +5,19 @@ function __init__()
 		println("You must have at least julia 0.4 to use this package")
 		println("You currently have ", VERSION)
 	end
+
+	try
+		Libdl.dlopen("libcsfml-graphics")
+	catch Exception
+		println("Something has gone wrong with the csfml installation. Please reinstall this package")
+	end
+
+	try
+		Libdl.dlopen("libsfml-graphics")
+	catch Exception
+		println("You must have the C++ library SFML installed to use this package")
+		println("Please install it")
+	end
 end
 
 include("julia/Window/keyboard.jl")
