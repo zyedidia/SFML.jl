d54829340163700339142736c7670984dfb17be9
Zachary Yedidia <zyedidia@gmail.com>
2015-04-11 14:30:38 -0400

    Change music

--- examples/Audio/music.jl
+++ examples/Audio/music.jl
@@ -1,28 +1,28 @@
 using SFML
 
-the_chicken = Music("TheChicken.ogg")
+music = Music("music.ogg")
 
-set_loop(the_chicken, true)
-play(the_chicken)
+set_loop(music, true)
+play(music)
 playing = true
 
-println("Press 'p' to pause and 'o' to resume")
+println("Press 'p' to pause and resume")
 println("Press escape to quit")
 
 while true
 	sleep(0.1)
 	
 	if is_key_pressed(KeyCode.ESCAPE)
-		stop(the_chicken)
-		destroy(the_chicken)
+		stop(music)
+		destroy(music)
 		exit(0)
 	end
 
 	if is_key_pressed(KeyCode.P)
 		if !playing
-			play(the_chicken)
+			play(music)
 		else
-			pause(the_chicken)
+			pause(music)
 		end
 		playing = !playing
 	end
