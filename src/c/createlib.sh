f0b8f84ddaab0a113daf328b6bf0dbccd156595d
Zachary Yedidia <zyedidia@gmail.com>
2015-04-17 11:30:52 -0400

    Check usr local

--- src/c/createlib.sh
+++ src/c/createlib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -fPIC -c Window/*.c Network/*.c System/*.c
+gcc -fPIC -I/usr/local/include -c Window/*.c Network/*.c System/*.c
 gcc -I/usr/local/include -L/usr/local/lib -lsfml-system -lsfml-graphics -lsfml-window -lsfml-audio -lsfml-network -lcsfml-system -lcsfml-graphics -lcsfml-window -lcsfml-audio -lcsfml-network -shared -o ../../deps/$1 *.o
 rm *.o
